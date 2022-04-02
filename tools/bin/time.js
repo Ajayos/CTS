48d07f2f5cf9af6290b4aeaff7a51bc09d26339b
Ajay-o-s <akhubsmaster@gmail.com>
2022-04-03 02:31:32 +0530

    👨‍💻CODE BY 🕊️★⃝AJAY O S©️🧚‍♂️
    👨‍💻CODE BY 🕊️★⃝AJAY O S©️🧚‍♂️

--- tools/bin/time.js
+++ tools/bin/time.js
@@ -1,32 +1,38 @@
-const express = require('express')
+const express = require('express');
 var router = express.Router();
-const axios = require('axios')
-__path = process.cwd()
-const fs = require('fs')
+const axios = require('axios');
+__path = process.cwd();
+const fs = require('fs');
 
 router.get('/', async(req, res) => {
-        const hrs = new Date().getHours({ timeZone: 'Asia/Kolkata' });
-        const min = new Date().getMinutes({ timeZone: 'Asia/Kolkata' });
-        const sec = new Date().getSeconds({ timeZone: 'Asia/Kolkata' });
-        const day = new Date().getDate({ timeZone: 'Asia/Kolkata' });
-        const mon = new Date().getMonth({ timeZone: 'Asia/Kolkata' });
-        const year = new Date().getFullYear({ timeZone: 'Asia/Kolkata' });
+        var d = new Date();
+        const hrs = d.getHours().toString().padStart(2, 0);
+        const min = d.getMinutes().toString().padStart(2, 0);
+        const sec = d.getSeconds().toString().padStart(2, 0);
+        const day = d.getDate();
+        const mon = d.getMonth();
+        const year = d.getFullYear();
 
-        gn_text = ["😘𝙂𝙤𝙤𝙙 🙈𝙣𝙞𝙜𝙝𝙩 💫✨","🤗𝓖𝓸𝓸𝓭 🧚‍♀𝓷𝓲𝓰𝓱𝓽 ❄️✨","🌌❡០០ᖱ 🌙⩎ɨ❡ϦƬ 🌎","😘ցօօժ ⭐️ղíցհԵ 💝","🌃Ꮐᝪᝪᗞ 🙈ᑎᏆᏀᕼᎢ 💫✨"];
-        gm_text = ["❀🍃Good❀ ❀morning❀🥰❀","☘️𝐺𝑜𝑜𝑑 🌅𝑚𝑜𝑟𝑛𝑖𝑛𝑔 💐","🍃𝙶𝚘𝚘𝚍 🌻𝚖𝚘𝚛𝚗𝚒𝚗𝚐 🥰","🍀𝗚𝗼𝗼𝗱 😘𝗺𝗼𝗿𝗻𝗶𝗻𝗴 🌸","🌻𝓖𝓸𝓸𝓭 𝓶𝓸𝓻𝓷𝓲𝓷𝓰 💞","🌼🅖🅞🅞🅓 🅜🅞🅡🅝🅘🅝🅖 🐶","🍃Ⓖⓞⓞⓓ 🌈ⓜⓞⓡⓝⓘⓝⓖ 🥰"];   
-	    var gn = Math.floor(5*Math.random());
-	    var gm = Math.floor(7*Math.random());
+        var gmgn = await axios.get('https://ajay-o-s.herokuapp.com/bot/gmgn');
+        
+		var gm = gmgn.data.gm;
+		var gf = gmgn.data.gf;
+		var ge = gmgn.data.ge;
+        var gn = gmgn.data.gn;
 
-        if (hrs < 12) wish = gm_text[gm]; //'ɢᴏᴏᴅ ᴍᴏʀɴɪɴɢ ⛅';
-        if (hrs >= 12 && hrs <= 17) wish = 'ɢᴏᴏᴅ ᴀғᴛᴇʀɴᴏᴏɴ 🌞';
-        if (hrs >= 17 && hrs <= 19) wish = 'ɢᴏᴏᴅ ᴇᴠᴇɴɪɴɢ 🌥';
-        if (hrs >= 19 && hrs <= 24) wish = gn_text[gn]; //'ɢᴏᴏᴅ ɴɪɢʜᴛ 🌙';
-        moths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+        if (hrs < 12) wish = gm;
+        if (hrs >= 12 && hrs <= 17) wish = gf;
+        if (hrs >= 17 && hrs <= 19) wish = ge;
+        if (hrs >= 19 && hrs <= 24) wish = gn;
+
+        moths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        hour = hrs==00 ? 0 : hrs==01 ? 1 : hrs==02 ? 2 : hrs==03 ? 3 : hrs==04 ? 4 : hrs==05 ? 5 : hrs==06 ? 6 : hrs==07 ? 7 : hrs==08 ? 8 : hrs==09 ? 9 : hrs; 
         arry = ["0","1","2","3","4","5","6","7","8","9","10","11","12","1","2","3","4","5","6","7","8","9","10","11","12"];
         ampm = hrs >= 12 ? 'PM' : 'AM';
+
 	res.json({
 		hr: hrs,
-        ihr: arry[hrs],
+        ihr: arry[hour],
         min: min,
         sec: sec,
         ampm: ampm,
