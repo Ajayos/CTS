f25b1de225e00e85725da118bacae21981e04eae
Ajay-o-s <akhubsmaster@gmail.com>
2022-04-05 17:46:23 +0530

    👨‍💻CODE BY 🕊️★⃝AJAY O S©️🧚‍♂️
    👨‍💻CODE BY 🕊️★⃝AJAY O S©️🧚‍♂️

--- js/bot.js
+++ js/bot.js
@@ -6,6 +6,7 @@ var router = express.Router();
 
 router.use('/gmgn', require(__path + '/tools/bin/gmgn.js'));
 router.use('/time', require(__path + '/tools/bin/time.js'));
+router.use('/startmsg', require(__path + '/tools/bin/startmsg.js'));
 router.get('/', async(req, res) => {res.sendFile(__path + '/pages/home.html')})
 //router.get('/gmgn', async(req, res) => {res.sendFile('')})
 //router.get('', async(req, res) => {res.sendFile('')})
