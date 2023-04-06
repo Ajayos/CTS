8b66d555ae7d842c2e77a95be5b173f535181467
Ajay o s <ajayosakhub@gmail.com>
2023-04-06 16:37:37 +0530

    update

--- Router/Home/index.js
+++ Router/Home/index.js
@@ -4,7 +4,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {res.json({server: "Started"})});
-router.get('/hi', function(req, res) {res.send('https://github.com/Ajay-o-s.png')});
-router.get('favicon.ico', function(req, res) {res.redirect('https://github.com/Ajay-o-s.png')});
+router.get('/hi', function(req, res) {res.send('https://github.com/Ajayos.png')});
+router.get('favicon.ico', function(req, res) {res.redirect('https://github.com/Ajayos.png')});
 
 module.exports = router;
--- Router/chat/page/index.js
+++ Router/chat/page/index.js
@@ -1,4 +1,4 @@
-const socket = io('http://192.168.148.37');
+const socket = io();
 const form = document.getElementById('send-container');
 const messageInput = document.getElementById('messageInp')
 const messageContainer = document.querySelector(".container")
