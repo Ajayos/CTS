d16d02f7436b3c160441d8aca8578eb2b68a545b
Ajay o s <ajayosakhub@gmail.com>
2023-06-25 14:01:46 +0530

    fixed #45

--- Services/User.js
+++ Services/User.js
@@ -32,91 +32,91 @@ const { ObjectId } = require("mongodb");
  * @returns {Object} Response indicating the status, error, message, and data of the login process.
  */
 exports.login = async (data) => {
-  try {
-    // Destructure the data object to get the required properties
-    const { email, password, device } = data;
+	try {
+		// Destructure the data object to get the required properties
+		const { email, password, device } = data;
 
-    // Find the user by email
-    const user = await User.findOne({ email });
+		// Find the user by email
+		const user = await User.findOne({ email });
 
-    // If user not found, return error
-    if (!user) {
-      return {
-        status: 404,
-        message: "User not found",
-        error: true,
-        data: undefined,
-      };
-    }
+		// If user not found, return error
+		if (!user) {
+			return {
+				status: 404,
+				message: "User not found",
+				error: true,
+				data: undefined,
+			};
+		}
 
-    // Compare the provided password with the hashed password in the database
-    const isMatch = await bcrypt.compare(password, user.password);
+		// Compare the provided password with the hashed password in the database
+		const isMatch = await bcrypt.compare(password, user.password);
 
-    // If passwords don't match, return error
-    if (!isMatch) {
-      return {
-        status: 401,
-        error: true,
-        message: "Invalid credentials",
-        data: undefined,
-      };
-    }
+		// If passwords don't match, return error
+		if (!isMatch) {
+			return {
+				status: 401,
+				error: true,
+				message: "Invalid credentials",
+				data: undefined,
+			};
+		}
 
-    // check access
-    if (!user.access) {
-      return {
-        status: 403,
-        error: true,
-        message: "Access Denied",
-        data: undefined,
-      };
-    }
+		// check access
+		if (!user.access) {
+			return {
+				status: 403,
+				error: true,
+				message: "Access Denied",
+				data: undefined,
+			};
+		}
 
-    // account delete
-    if (user.delete) {
-      return {
-        status: 204,
-        error: true,
-        message: "Account Deleted!",
-        data: undefined,
-      };
-    }
+		// account delete
+		if (user.delete) {
+			return {
+				status: 204,
+				error: true,
+				message: "Account Deleted!",
+				data: undefined,
+			};
+		}
 
-    // Generate and return a token for authentication
-    const token = encode(user);
-    if (!token)
-      return {
-        status: 500,
-        error: true,
-        message: "Internal server error",
-        data: undefined,
-      };
+		// Generate and return a token for authentication
+		const token = encode(user);
+		if (!token)
+			return {
+				status: 500,
+				error: true,
+				message: "Internal server error",
+				data: undefined,
+			};
 
-    const tokenID = uuidv4();
-    user.token.push({
-      id: tokenID,
-      token: token,
-      lastLogin: Date.now(),
-      created: Date.now(),
-      device: device,
-      status: "active",
-    });
-    await user.save();
+		const tokenID = uuidv4();
+		user.token.push({
+			id: tokenID,
+			token: token,
+			lastLogin: Date.now(),
+			created: Date.now(),
+			device: device,
+			status: "active",
+		});
+		await user.save();
 
-    return {
-      status: 200,
-      error: false,
-      message: undefined,
-      data: { token, user },
-    };
-  } catch (error) {
-    return {
-      status: 500,
-      error: true,
-      message: "Internal server error",
-      data: undefined,
-    };
-  }
+		return {
+			status: 200,
+			error: false,
+			message: undefined,
+			data: { token, user },
+		};
+	} catch (error) {
+		return {
+			status: 500,
+			error: true,
+			message: "Internal server error",
+			data: undefined,
+		};
+	}
 };
 
 /**
@@ -137,81 +137,81 @@ exports.login = async (data) => {
  * @returns {Object} Response indicating the status, error, message, and data of the account creation process.
  */
 exports.createAccount = async (data) => {
-  try {
-    // Destructure the data object to get the required properties
-    const { email, password, pic, name, dob, age, phoneNo, place, device } =
-      data;
+	try {
+		// Destructure the data object to get the required properties
+		const { email, password, pic, name, dob, age, phoneNo, place, device } =
+			data;
 
-    // Check if the user already exists
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return {
-        status: 409,
-        error: true,
-        message: "User already exists",
-        data: undefined,
-      };
-    }
+		// Check if the user already exists
+		const existingUser = await User.findOne({ email });
+		if (existingUser) {
+			return {
+				status: 409,
+				error: true,
+				message: "User already exists",
+				data: undefined,
+			};
+		}
 
-    // Hash the password
-    const hashedPassword = await bcrypt.hash(password, 10);
+		// Hash the password
+		const hashedPassword = await bcrypt.hash(password, 10);
 
-    // Create the new user account
-    const newUser = new User({
-      email,
-      password: hashedPassword,
-      name,
-      pic,
-      dob,
-      age,
-      phoneNumber: phoneNo,
-      place,
-      access: true,
-      created: Date.now(),
-      status: "active",
-    });
+		// Create the new user account
+		const newUser = new User({
+			email,
+			password: hashedPassword,
+			name,
+			pic,
+			dob,
+			age,
+			phoneNumber: phoneNo,
+			place,
+			access: true,
+			created: Date.now(),
+			status: "active",
+		});
 
-    await newUser.save();
+		await newUser.save();
 
-    const user = await User.findOne({ email });
-    // Generate and return a token for authentication
-    const token = encode(user);
-    if (!token)
-      return {
-        status: 500,
-        error: true,
-        message: "Internal server error",
-        data: undefined,
-      };
+		const user = await User.findOne({ email });
+		// Generate and return a token for authentication
+		const token = encode(user);
+		if (!token)
+			return {
+				status: 500,
+				error: true,
+				message: "Internal server error",
+				data: undefined,
+			};
 
-    const tokenID = uuidv4();
-    user.token.push({
-      id: tokenID,
-      token: token,
-      lastLogin: Date.now(),
-      created: Date.now(),
-      device: device,
-      status: "active",
-    });
-    await user.save();
+		const tokenID = uuidv4();
+		user.token.push({
+			id: tokenID,
+			token: token,
+			lastLogin: Date.now(),
+			created: Date.now(),
+			device: device,
+			status: "active",
+		});
+		await user.save();
 
-    return {
-      status: 200,
-      error: false,
-      message: undefined,
-      data: {
-        token,
-        user,
-      },
-    };
-  } catch (error) {
-    return {
-      status: 500,
-      error: true,
-      message: "Internal server error",
-      data: undefined,
-    };
-  }
+		return {
+			status: 200,
+			error: false,
+			message: undefined,
+			data: {
+				token,
+				user,
+			},
+		};
+	} catch (error) {
+		return {
+			status: 500,
+			error: true,
+			message: "Internal server error",
+			data: undefined,
+		};
+	}
 };
 
 /**
@@ -223,64 +223,63 @@ exports.createAccount = async (data) => {
  * @returns {Object} Response indicating the status, error, message, and data of the password update process.
  */
 exports.updatePassword = async (data) => {
-  try {
-    const { id, password, device } = data;
+	try {
+		const { id, password, device } = data;
 
-    // Find the user by ID
-    const user = await User.findById(id);
+		// Find the user by ID
+		const user = await User.findById(id);
 
-    if (!user) {
-      return {
-        status: 404,
-        error: true,
-        message: "User not found",
-        data: undefined,
-      };
-    }
+		if (!user) {
+			return {
+				status: 404,
+				error: true,
+				message: "User not found",
+				data: undefined,
+			};
+		}
 
-    // Hash the password
-    const hashedPassword = await bcrypt.hash(password, 10);
-    user.password = hashedPassword;
-    await user.save();
+		// Hash the password
+		const hashedPassword = await bcrypt.hash(password, 10);
+		user.password = hashedPassword;
+		await user.save();
 
-    const user_ = await User.findById(id);
-    // Generate and return a token for authentication
-    const token = encode(user_);
-    if (!token)
-      return {
-        status: 500,
-        error: true,
-        message: "Internal server error",
-        data: undefined,
-      };
+		const user_ = await User.findById(id);
+		// Generate and return a token for authentication
+		const token = encode(user_);
+		if (!token)
+			return {
+				status: 500,
+				error: true,
+				message: "Internal server error",
+				data: undefined,
+			};
 
-    const tokenID = uuidv4();
-    user_.token.push({
-      id: tokenID,
-      token: token,
-      lastLogin: Date.now(),
-      created: Date.now(),
-      device: device,
-      status: "active",
-    });
-    await user_.save();
+		const tokenID = uuidv4();
+		user_.token.push({
+			id: tokenID,
+			token: token,
+			lastLogin: Date.now(),
+			created: Date.now(),
+			device: device,
+			status: "active",
+		});
+		await user_.save();
 
-    return {
-      status: 200,
-      error: false,
-      message: "Password updated successfully",
-      data: { token, user },
-    };
-  } catch (error) {
-    return {
-      status: 500,
-      error: true,
-      message: "Internal server error",
-      data: undefined,
-    };
-  }
+		return {
+			status: 200,
+			error: false,
+			message: "Password updated successfully",
+			data: { token, user },
+		};
+	} catch (error) {
+		return {
+			status: 500,
+			error: true,
+			message: "Internal server error",
+			data: undefined,
+		};
+	}
 };
-
 
 /**
  * Update user password.
@@ -292,70 +291,70 @@ exports.updatePassword = async (data) => {
  * @returns {Object} Response indicating the status, error, message, and data of the password update process.
  */
 exports.forgotPassword = async (data) => {
-  try {
-    const { email, dob, password } = data;
+	try {
+		const { email, dob, password } = data;
 
-    // Find the user by ID
-    const user = await User.findOne({ email });
+		// Find the user by ID
+		const user = await User.findOne({ email });
 
-    if (!user) {
-      return {
-        status: 404,
-        error: true,
-        message: "User not found",
-        data: undefined,
-      };
-    }
+		if (!user) {
+			return {
+				status: 404,
+				error: true,
+				message: "User not found",
+				data: undefined,
+			};
+		}
 
-    if (user.dob !== dob) {
-      return {
-        status: 409,
-        error: true,
-        message: "Dob don't match",
-        data: undefined,
-      };
-    }
+		if (user.dob !== dob) {
+			return {
+				status: 409,
+				error: true,
+				message: "Dob don't match",
+				data: undefined,
+			};
+		}
 
-    // check access
-    if (!user.access) {
-      return {
-        status: 403,
-        error: true,
-        message: "Access Denied",
-        data: undefined,
-      };
-    }
+		// check access
+		if (!user.access) {
+			return {
+				status: 403,
+				error: true,
+				message: "Access Denied",
+				data: undefined,
+			};
+		}
 
-    // account delete
-    if (user.delete) {
-      return {
-        status: 204,
-        error: true,
-        message: "Account Deleted!",
-        data: undefined,
-      };
-    }
+		// account delete
+		if (user.delete) {
+			return {
+				status: 204,
+				error: true,
+				message: "Account Deleted!",
+				data: undefined,
+			};
+		}
 
-    // Hash the password
-    const hashedPassword = await bcrypt.hash(password, 10);
+		// Hash the password
+		const hashedPassword = await bcrypt.hash(password, 10);
 
-    user.password = hashedPassword;
-    await user.save();
+		user.password = hashedPassword;
+		await user.save();
 
-    return {
-      status: 200,
-      error: false,
-      message: "Password updated successfully",
-      data: { message: "Password updated successfully" },
-    };
-  } catch (error) {
-    return {
-      status: 500,
-      error: true,
-      message: "Internal server error",
-      data: undefined,
-    };
-  }
+		return {
+			status: 200,
+			error: false,
+			message: "Password updated successfully",
+			data: { message: "Password updated successfully" },
+		};
+	} catch (error) {
+		return {
+			status: 500,
+			error: true,
+			message: "Internal server error",
+			data: undefined,
+		};
+	}
 };
 
 /**
@@ -370,42 +369,42 @@ exports.forgotPassword = async (data) => {
  * @returns {Object} Response indicating the status, error, message, and data of the process.
  */
 exports.editAccount = async (data) => {
-  try {
-    const { id, name, dob, age, place } = data;
+	try {
+		const { id, name, dob, age, place } = data;
 
-    // Find the user by ID
-    const user = await User.findById(id);
+		// Find the user by ID
+		const user = await User.findById(id);
 
-    if (!user) {
-      return {
-        status: 404,
-        error: true,
-        message: "User not found",
-        data: undefined,
-      };
-    }
+		if (!user) {
+			return {
+				status: 404,
+				error: true,
+				message: "User not found",
+				data: undefined,
+			};
+		}
 
-    // Update the user account with the provided data
-    user.name = name;
-    user.dob = dob;
-    user.age = age;
-    user.place = place;
-    await user.save();
+		// Update the user account with the provided data
+		user.name = name;
+		user.dob = dob;
+		user.age = age;
+		user.place = place;
+		await user.save();
 
-    return {
-      status: 200,
-      error: false,
-      message: "User account updated successfully",
-      data: { message: "User account updated successfully" },
-    };
-  } catch (error) {
-    return {
-      status: 500,
-      error: true,
-      message: "Internal server error",
-      data: undefined,
-    };
-  }
+		return {
+			status: 200,
+			error: false,
+			message: "User account updated successfully",
+			data: { message: "User account updated successfully" },
+		};
+	} catch (error) {
+		return {
+			status: 500,
+			error: true,
+			message: "Internal server error",
+			data: undefined,
+		};
+	}
 };
 
 /**
@@ -417,41 +416,43 @@ exports.editAccount = async (data) => {
  * @returns {Object} Response indicating the status, error, message, and data of the user access update process.
  */
 exports.editAccess = async (data) => {
-  try {
-    const { id, access } = data;
+	try {
+		const { id, access } = data;
 
-    // Find the user by ID
-    const user = await User.findById(id);
-    if (!user) {
-      return {
-        status: 404,
-        error: true,
-        message: "User not found",
-        data: undefined,
-      };
-    }
+		// Find the user by ID
+		const user = await User.findById(id);
+		if (!user) {
+			return {
+				status: 404,
+				error: true,
+				message: "User not found",
+				data: undefined,
+			};
+		}
+		var blocked = "blocked";
+		var inactive = "inactive";
 
-    // Update the user access with the provided data
-    if (access != user.access) {
-      user.access = access;
-      user.status = access ? "blocked" : "inactive";
-      await user.save();
-    }
+		// Update the user access with the provided data
+		if (access != user.access) {
+			user.access = access;
+			user.status = access ? blocked : inactive;
+			await user.save();
+		}
 
-    return {
-      status: 200,
-      error: false,
-      message: "User access updated successfully",
-      data: { message: "User account updated successfully" },
-    };
-  } catch (error) {
-    return {
-      status: 500,
-      error: true,
-      message: "Internal server error",
-      data: undefined,
-    };
-  }
+		return {
+			status: 200,
+			error: false,
+			message: "User access updated successfully",
+			data: { message: "User account updated successfully" },
+		};
+	} catch (error) {
+		return {
+			status: 500,
+			error: true,
+			message: "Internal server error",
+			data: undefined,
+		};
+	}
 };
 
 /**
@@ -462,48 +463,48 @@ exports.editAccess = async (data) => {
  * @returns {Object} Response indicating the status, error, message, and data of the process.
  */
 exports.deleteAccount = async (data) => {
-  try {
-    // Destructure the data object to get the required properties
-    const { id } = data;
+	try {
+		// Destructure the data object to get the required properties
+		const { id } = data;
 
-    // Find the user by ID
-    const user = await User.findById(id);
+		// Find the user by ID
+		const user = await User.findById(id);
 
-    // If user not found, return error
-    if (!user) {
-      return {
-        status: 404,
-        message: "User not found",
-        error: true,
-        data: undefined,
-      };
-    }
+		// If user not found, return error
+		if (!user) {
+			return {
+				status: 404,
+				message: "User not found",
+				error: true,
+				data: undefined,
+			};
+		}
 
-    if (user.delete) {
-      user.delete = false;
-      user.access = true;
-      user.status = "inactive";
-    } else {
-      user.delete = true;
-      user.access = false;
-      user.status = "deleted";
-      await user.save();
-    }
+		if (user.delete) {
+			user.delete = false;
+			user.access = true;
+			user.status = "inactive";
+		} else {
+			user.delete = true;
+			user.access = false;
+			user.status = "deleted";
+			await user.save();
+		}
 
-    return {
-      status: 200,
-      error: false,
-      message: "User account deleted successfully",
-      data: { message: "User account deleted successfully" },
-    };
-  } catch (error) {
-    return {
-      status: 500,
-      error: true,
-      message: "Internal server error",
-      data: undefined,
-    };
-  }
+		return {
+			status: 200,
+			error: false,
+			message: "User account deleted successfully",
+			data: { message: "User account deleted successfully" },
+		};
+	} catch (error) {
+		return {
+			status: 500,
+			error: true,
+			message: "Internal server error",
+			data: undefined,
+		};
+	}
 };
 
 /**
@@ -515,36 +516,36 @@ exports.deleteAccount = async (data) => {
  * @returns {Object} Response indicating the status, error, message, and data of the process.
  */
 exports.editAccountPic = async (data) => {
-  try {
-    const { id, pic } = data;
+	try {
+		const { id, pic } = data;
 
-    // Find the user by ID
-    const user = await User.findById(id);
+		// Find the user by ID
+		const user = await User.findById(id);
 
-    if (!user) {
-      return {
-        status: 404,
-        error: true,
-        message: "User not found",
-        data: undefined,
-      };
-    }
-    user.pic = pic;
+		if (!user) {
+			return {
+				status: 404,
+				error: true,
+				message: "User not found",
+				data: undefined,
+			};
+		}
+		user.pic = pic;
 
-    await user.save();
+		await user.save();
 
-    return {
-      status: 200,
-      error: false,
-      message: "User account updated successfully",
-      data: { message: "User account updated successfully" },
-    };
-  } catch (error) {
-    return {
-      status: 500,
-      error: true,
-      message: "Internal server error",
-      data: undefined,
-    };
-  }
+		return {
+			status: 200,
+			error: false,
+			message: "User account updated successfully",
+			data: { message: "User account updated successfully" },
+		};
+	} catch (error) {
+		return {
+			status: 500,
+			error: true,
+			message: "Internal server error",
+			data: undefined,
+		};
+	}
 };
