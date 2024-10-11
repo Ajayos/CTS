ec350bea624064ca56fdcfe2f3b74fc9c5be396b
Ajay o s <ajayosakhub@gmail.com>
2024-10-11 10:18:44 +0530

    Remove unnecessary comments from webhook route and simplify test.txt content

--- src/Routes/webhook.v1.ts
+++ src/Routes/webhook.v1.ts
@@ -1,14 +1,3 @@
-/**
- * @module src/Routes/webhook.v1
- * @create_date 19-08-2024
- * @last_update 19-08-2024
- * @created_by Ajay o s
- * @last_updated_by Ajay o s
- * @version 1.0.0
- * @description Webhook route module
- *
- */
-
 import { Request, Response, Router } from 'express';
 const router = Router();
 
